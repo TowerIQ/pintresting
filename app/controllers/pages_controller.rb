e89a183fcf04f561754a4abfe63311fe8f52b102
Adam Demos <adam@springfieldangels.com>
2014-12-10 15:30:12 -0500

    Pin Style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
 
   def About
   end
+
+  def howitworks
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 root "pages#home"
 
-get "about" => "pages#about" #creates about_path
+get "about" => "pages#about"
+
+get "howitworks" => "pages#howitworks" 
+
+#creates about_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
