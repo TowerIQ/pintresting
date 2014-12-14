2b02e3bcbf1cd9d0bebb9b37efe4912a35806cee
Adam Demos <adam@springfieldangels.com>
2014-12-14 18:14:02 -0500

    add users associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141213042243) do
+ActiveRecord::Schema.define(version: 20141214221659) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
